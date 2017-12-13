1faf56a009cc6c4ecdfef03963fcd00edda9c4b2
Michael Bates <mklbtz@gmail.com>
2017-12-13 02:32:31 -0500

    refactor things a bit

--- Sources/Advent/DayOne.swift
+++ Sources/Advent/DayOne.swift
@@ -1,31 +1,28 @@
 public func dayOneA(_ string: String) -> Int {
     guard string.count > 1 else { return 0 }
-    let first = string.first!
-    var sum = 0
-    if string.last == first {
-        sum += Int(String(first))!
-    }
-    for index in string.indices.dropFirst()
-    where string[index] == string[string.index(before: index)] {
-            sum += Int(String(string[index]))!
+    var sum = check(string.dropFirst(), string.dropLast())
+    if string.last == string.first {
+        sum += Int(string.first!)
     }
     return sum
 }
 
 public func dayOneB(_ string: String) -> Int {
     guard string.count > 1 else { return 0 }
-    var sum = 0
     let halfIndex = string.halfIndex
-    for (c, r) in zip(string[..<halfIndex], string[halfIndex...]) where c == r {
-        let num = Int(String(c))!
-        sum += num
-    }
-    return sum * 2
+    return check(string[..<halfIndex], string[halfIndex...]) * 2
 }
-    
+
+func check(_ lhs: Substring, _ rhs: Substring) -> Int {
+    var sum = 0
+    for (l, r) in zip(lhs, rhs) where l == r {
+        sum += Int(l)
+    }
+    return sum
+}
 
 extension Collection {
-    public var halfIndex: Index {
+    var halfIndex: Index {
         if count.magnitude <= 1 {
             return endIndex
         } else {
@@ -33,3 +30,10 @@ extension Collection {
         }
     }
 }
+
+extension Int {
+    init(_ char: Character) {
+        self = Int(String(char)) ?? 0
+    }
+}
+
