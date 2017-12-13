652458cf50923c013b3eede82e8d8ac60e47c78e
Michael Bates <mklbtz@gmail.com>
2017-12-13 02:08:31 -0500

    submit day one part B

--- Sources/Advent/DayOne.swift
+++ Sources/Advent/DayOne.swift
@@ -6,8 +6,30 @@ public func dayOneA(_ string: String) -> Int {
         sum += Int(String(first))!
     }
     for index in string.indices.dropFirst()
-        where string[index] == string[string.index(before: index)] {
+    where string[index] == string[string.index(before: index)] {
             sum += Int(String(string[index]))!
     }
     return sum
+}
+
+public func dayOneB(_ string: String) -> Int {
+    guard string.count > 1 else { return 0 }
+    var sum = 0
+    let halfIndex = string.halfIndex
+    for (c, r) in zip(string[..<halfIndex], string[halfIndex...]) where c == r {
+        let num = Int(String(c))!
+        sum += num
+    }
+    return sum * 2
+}
+    
+
+extension Collection {
+    public var halfIndex: Index {
+        if count.magnitude <= 1 {
+            return endIndex
+        } else {
+            return index(startIndex, offsetBy: count / 2)
+        }
+    }
 }
