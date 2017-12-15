abe0245c20cc829a5c95729d9db3449cd186049a
Michael Bates <mklbtz@gmail.com>
2017-12-15 13:08:14 -0500

    introduce Sector enum
    represents the various kinds of places a set of coordinates can be. Used by forward and backward as an abstraction over all the math I was doing before. makes intent much clearer in those functions and removes the need for comments.

--- Sources/Advent/DayThree.swift
+++ Sources/Advent/DayThree.swift
@@ -5,15 +5,25 @@ func dayThreeA(_ iterations: Int) -> Int {
 }
 
 struct Spiral {
-  enum Direction {
-    case up, down, left, right
-  }
-
   struct Index {
     var x: Int
     var y: Int
 
     static var origin: Index = Index(x: 0, y: 0)
+  }
+
+  enum Direction {
+    case north, south, west, east
+  }
+
+  enum Diagonal {
+    case northEast, northWest, southEast, southWest
+  }
+
+  enum Sector {
+    case origin
+    case wedge(Direction)
+    case diagonal(Diagonal)
   }
 }
 
@@ -46,60 +56,60 @@ extension Spiral.Index {
 
   private func moving(_ direction: Spiral.Direction) -> Spiral.Index {
     switch direction {
-    case .up:
+    case .north:
       return Spiral.Index(x: x, y: y + 1)
-    case .down:
+    case .south:
       return Spiral.Index(x: x, y: y - 1)
-    case .left:
+    case .west:
       return Spiral.Index(x: x - 1, y: y)
-    case .right:
+    case .east:
       return Spiral.Index(x: x + 1, y: y)
     }
   }
 
+  var sector: Spiral.Sector {
+    return .init(x: x, y: y)
+  }
+
+
   var forward: Spiral.Direction {
-    if x < y && -x < y { // north wedge
-      return .left
-    } else if x < -y && x < y { // west wedge
-      return .down
-    } else if x > y && x < -y { // south wedge
-      return .right
-    } else if -x < y && x > y { // east wedge
-      return .up
-    } else if -x == y && x >= 0 { // quad 4 diagonal and origin
-      return .right // move into next layer of spiral
-    } else if -x == y && x < 0 { // quad 2 diagonal
-      return .down
-    } else if x == y && x > 0 { // quad 1 diagonal
-      return .left
-    } else if x == y && x < 0 { // quad 3 diagonal
-      return .right
-    } else {
-      fatalError("This should be exhaustive!")
+    switch sector {
+    case .origin,
+         .wedge(.south),
+         .diagonal(.southWest),
+         .diagonal(.southEast): // move into next layer of spiral
+      return .east
+    case .wedge(.east):
+      return .north
+    case .wedge(.north), .diagonal(.northEast):
+      return .west
+    case .wedge(.west), .diagonal(.northWest):
+      return .south
     }
   }
 
   var backward: Spiral.Direction {
-    if self == .origin {
+    switch sector {
+    case .origin:
       fatalError("No index before origin")
-    } else if x < y && -x < y { // north wedge
-      return .right
-    } else if x < -y && x < y { // west wedge
-      return .up
-    } else if x > y && x <= -y { // south wedge & quad 4 diagonal
-      return .left
-    } else if -x < y && x > y { // east wedge
-      return .down
-    } else if -x-1 == y && x > 0 { // just right of quad 4 diagonal
-      return .left // move into previous layer of spiral
-    } else if -x == y && x < 0 { // quad 2 diagonal
-      return .right
-    } else if x == y && x > 0 { // quad 1 diagonal
-      return .down
-    } else if x == y && x < 0 { // quad 3 diagonal
-      return .up
-    } else {
-      fatalError("This should be exhaustive!")
+    case .wedge(.north):
+      return .east
+    case .wedge(.west):
+      return .north
+    case .wedge(.south):
+      return .west
+    case .wedge(.east) where y == (-x - 1):
+      return .west // move into previous layer of spiral
+    case .wedge(.east):
+      return .south
+    case .diagonal(.northEast):
+      return .south
+    case .diagonal(.northWest):
+      return .east
+    case .diagonal(.southWest):
+      return .north
+    case .diagonal(.southEast):
+      return .west
     }
   }
 }
@@ -107,5 +117,31 @@ extension Spiral.Index {
 extension Spiral.Index: Equatable {
   static func ==(lhs: Spiral.Index, rhs: Spiral.Index) -> Bool {
     return lhs.x == rhs.x && lhs.y == rhs.y
+  }
+}
+
+extension Spiral.Sector {
+  init(x: Int, y: Int) {
+    if x == 0 , y == 0 {
+      self = .origin
+    } else if x < y && -x < y {
+      self = .wedge(.north)
+    } else if x < -y && x < y {
+      self = .wedge(.west)
+    } else if x > y && x < -y {
+      self = .wedge(.south)
+    } else if -x < y && x > y {
+      self = .wedge(.east)
+    } else if x == y && x > 0 {
+      self = .diagonal(.northEast)
+    } else if -x == y && x < 0 {
+      self = .diagonal(.northWest)
+    } else if x == y && x < 0 {
+      self = .diagonal(.southWest)
+    } else if -x == y && x > 0 {
+      self = .diagonal(.southEast)
+    } else {
+      fatalError("This should be exhaustive!")
+    }
   }
 }
